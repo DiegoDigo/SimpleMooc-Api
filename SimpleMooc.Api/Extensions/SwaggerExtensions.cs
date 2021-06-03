4f66b55028861b714040da0fb3f1da3e1871e56f
Diego Domingos Delmiro <diego.delmiro@totvs.com.br>
2021-06-03 10:54:00 -0300

    fix ajusta na documentação

--- SimpleMooc.Api/Extensions/SwaggerExtensions.cs
+++ SimpleMooc.Api/Extensions/SwaggerExtensions.cs
@@ -12,7 +12,18 @@ namespace SimpleMooc.Api.Extensions
         {
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "SimpleMooc.Api", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "SimpleMooc.Api",
+                    Version = "v1",
+                    Description = "Simple api de cursos.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Diego Domingos Delmiro",
+                        Email = "di3g0d0ming05@gmail.com",
+                        Url = new Uri("https://github.com/DiegoDigo"),
+                    },
+                });
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
